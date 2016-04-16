ee65f6df6ff7df9acf99dbe0ca215558ebbf19a8
hightoon <haitong.chen@gmail.com>
2016-04-16 11:22:37 +0800

    changed root dir

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://112.74.68.197:8000/tm/';
+$config['base_url'] = 'http://112.74.68.197:8000/';
 
 /*
 |--------------------------------------------------------------------------
